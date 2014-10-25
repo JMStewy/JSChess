d93cbdee5393be183891ab78df74d7ae6f675b90
georgejf <george.jing.fu@gmail.com>
2014-10-25 13:54:43 -0400

    run the test

--- js/application.js
+++ js/application.js
@@ -3,4 +3,6 @@ var application = angular.module('JSChess', []);
 application.controller('IndexController', function($scope) {
     $scope.greeting = 'hello';
     $scope.game = new Chess.Game();
+
+    $scope.game.test();
 });
